606b2f42ce0750d0c47f70552292e59e301fd707
Suraj Rana <surajrana52@gmail.com>
2019-07-14 02:52:15 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Stringto-object
+
+> Make nested javascript object from string representation.
+
+## Installation
+
+```
+npm i @backendsuraj/stringto-object --save
+```
+
+## Usage
+
+```javascript
+const string2object = require('@backendsuraj/stringto-object');
+
+string2object.getObject("users.details.phone",'91987***321')
+// { users: { details: { phone: '91987***321' } } }
+
+//using for loop
+let elements = [{
+    "user_phone": {
+        "object_definition": {
+            "string": "users.details.phone",
+            "data": "91987***321"
+        }
+    },
+    "user_address": {
+        "object_definition": {
+            "string": "users.details.address",
+            "data": "Delhi, India"
+        }
+    },
+}];
+
+let objectHolder = {};
+
+for (let [key, value] of Object.entries(elements[0])) {
+    string2object.getObjectWithHolder(objectHolder, value.object_definition.string, value.object_definition.data);
+}
+
+console.log(objectHolder);
+//{ users: { details: { phone: '91987***321', address: 'Delhi, India' } } }
+
+```
+
+### Methods
+
+* **string2object.getObject("hello.world.reply","HI")**
+* **string2object.getObjectWithHolder(ObjectHolder = {},"hello.world.reply","HI")**
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,79 @@
+/**
+ *
+ * @param outputHolder
+ * @param objDefinition
+ * @param objValue
+ * @returns {*}
+ */
+function getObjectCustom(outputHolder, objDefinition, objValue) {
+
+    if (validateString(objDefinition)) {
+
+        makeObjectFromString(outputHolder, objDefinition, objValue);
+
+        return outputHolder;
+
+    } else {
+        throw new TypeError("Invalid string passed.");
+    }
+}
+
+/**
+ *
+ * @param objDefinition
+ * @param objValue
+ */
+function getObject(objDefinition, objValue) {
+
+    if (validateString(objDefinition)) {
+
+        let outputHolder = {};
+
+        makeObjectFromString(outputHolder, objDefinition, objValue);
+
+        return outputHolder;
+
+    } else {
+        throw new TypeError("Invalid string passed.");
+    }
+}
+
+
+
+/**
+ *
+ * @param string
+ * @returns {boolean}
+ */
+function validateString(string) {
+
+    if (typeof string === "string") {
+
+        let regex = /^(\w+.)*\w+$/;
+
+        return regex.test(string);
+
+    } else {
+        return false;
+    }
+}
+
+/**
+ *
+ * @param outputHolder
+ * @param definition
+ * @param value
+ */
+function makeObjectFromString(outputHolder, definition, value) {
+
+    const splitSting = definition.split('.');
+    const lastKey = splitSting.pop();
+    const lastObj = splitSting.reduce((obj, key) => obj[key] = obj[key] || {}, outputHolder);
+    lastObj[lastKey] = value;
+}
+
+
+module.exports = {
+    getObject: getObject,
+    getObjectWithHolder: getObjectCustom
+};
